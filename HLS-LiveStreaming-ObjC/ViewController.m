784b8dda7650df3a9174ac5f47b98e08ed4fec89
Alistair Cooper <cooper@swiftcodecomposer.com>
2016-06-12 16:20:37 -0700

    Initial Commit

---
+++ HLS-LiveStreaming-ObjC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HLS-LiveStreaming-ObjC
+//
+//  Created by Alistair Cooper on 6/12/16.
+//  Copyright © 2016 Alistair Cooper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
